b2a7eb431852330165d71c1b1f4b18aac9153eb2
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-08-15 21:54:57 -0700

    Add length and clear functions to hash map

--- src/hashmap.js
+++ src/hashmap.js
@@ -4,11 +4,13 @@ import { LinkedList } from "./linkedlist.js";
 //   throw new Error("Trying to access index out of bound");
 // }
 //
-function HashMap() {
+export function HashMap() {
   let buckets = Array(16);
   function getBuckets() {
     buckets.forEach((element)=>{
-      console.log(element.getHead())
+      if (element !== undefined){
+        console.log(element.getHead())
+      }
     })
     return buckets;
   }
@@ -101,54 +103,24 @@ function HashMap() {
     correctBucket.removeAt(index);
     return true;
   }
+  function length(){
+    let length = 0
+    for (let i=0; i<buckets.length; i++) {
+      if(buckets[i] !== undefined){
+        length+=buckets[i].size();
+      }
+    }
+    return length;
+  }
+  function clear() {
+    for (let i=0; i<buckets.length; i++) {
+      if(buckets[i] !== undefined){
+        delete buckets[i]
+      }
+    }
+  }
   
 
-  return {getBuckets, hash, set, get, has, remove}
+  return {getBuckets, hash, set, get, has, remove, length, clear}
 
 }
-let hash = HashMap();
-hash.set("hi", "you");
-hash.set("hi", "hi");
-hash.set("oy", "afd");
-hash.set("yamate", "adfaff");
-hash.set("carriba", "ggadf");
-hash.set("norm", "daggd");
-hash.set("hello", "teehee");
-hash.set("groan", "yahpp")
-hash.set("lalala", "nawwww")
-hash.set("omg", "bithc");
-hash.set("adgagad", "dagfgd");
-hash.set("opoppoodidk", "daggd");
-hash.set("adfaggggggeer", "daggd");
-hash.set("pppdaofoiugjhea", "daggd");
-hash.set("t", "daggd");
-hash.set("dooooff[[[[d", "daggd");
-hash.set("lllkkdkkdd", "daggd");
-hash.set("tttrriiirrir", "daggd");
-hash.set("llkfjhfhgyie", "daggd");
-hash.set("fioshriavgf", "daggd");
-hash.set("lorem", "daggd");
-hash.set("ipsum", "daggd");
-hash.set("goootthadfa", "daggd");
-hash.set("poiurekd", "daggd");
-hash.set("woepriurbasdfa", "daggd");
-hash.set("liabbdfope", "daggd");
-hash.set("qwuwur", "daggd");
-hash.set("3213123", "daggd");
-hash.set("2", "daggd");
-hash.set("eaiieiuegwiub", "daggd");
-hash.set("pppihabeiubiug", "daggd");
-hash.set("feeieieieieie", "daggd");
-hash.set("hi", "boyo");
-hash.set("groan", "teehee");
-hash.set("groan", "nonono");
-console.log(hash.getBuckets());
-console.log(hash.get("groan"))
-console.log(hash.get("qwuwur"))
-console.log(hash.has("waffle"))
-
-console.log(hash.has("groan"))
-console.log(hash.get("groan"))
-hash.remove("groan");
-console.log(hash.has("groan"))
-console.log(hash.get("groan"))
--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
 import './style.css';
-import { HashMap } from './hashmap';
+import { HashMap } from './hashmap.js';
+
+let hash = HashMap();
+hash.set("hi", "you");
+console.log(hash.length())
+hash.set("hi", "hi");
+hash.set("oy", "afd");
+hash.set("yamate", "adfaff");
+hash.set("carriba", "ggadf");
+hash.set("norm", "daggd");
+hash.set("hello", "teehee");
+hash.set("groan", "yahpp")
+hash.set("lalala", "nawwww")
+hash.set("omg", "bithc");
+hash.set("adgagad", "dagfgd");
+hash.set("opoppoodidk", "daggd");
+hash.set("adfaggggggeer", "daggd");
+hash.set("pppdaofoiugjhea", "daggd");
+hash.set("t", "daggd");
+hash.set("dooooff[[[[d", "daggd");
+hash.set("lllkkdkkdd", "daggd");
+hash.set("tttrriiirrir", "daggd");
+hash.set("llkfjhfhgyie", "daggd");
+hash.set("fioshriavgf", "daggd");
+hash.set("lorem", "daggd");
+hash.set("ipsum", "daggd");
+hash.set("goootthadfa", "daggd");
+hash.set("poiurekd", "daggd");
+hash.set("woepriurbasdfa", "daggd");
+hash.set("liabbdfope", "daggd");
+hash.set("qwuwur", "daggd");
+hash.set("3213123", "daggd");
+hash.set("2", "daggd");
+hash.set("eaiieiuegwiub", "daggd");
+hash.set("pppihabeiubiug", "daggd");
+hash.set("feeieieieieie", "daggd");
+hash.set("hi", "boyo");
+hash.set("groan", "teehee");
+hash.set("groan", "nonono");
+console.log(hash.getBuckets());
+console.log(hash.get("groan"))
+console.log(hash.get("qwuwur"))
+console.log(hash.has("waffle"))
+
+console.log(hash.has("groan"))
+console.log(hash.get("groan"))
+console.log(hash.length())
+hash.remove("groan");
+console.log(hash.has("groan"))
+console.log(hash.get("groan"))
+console.log(hash.length())
+hash.clear();
+console.log(hash.getBuckets());
+hash.set("hi", "you");
+console.log(hash.getBuckets());
